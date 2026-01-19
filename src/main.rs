3fc6a5c6e1f8a05073a645cbd9cffbed5e2d79b9
Christoph Lürig <christoph.luerig@gmail.com>
2026-01-19 14:37:57 +0100

    first version working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![windows_subsystem = "windows"]
 
-use macroquad::prelude::{clear_background, get_frame_time, next_frame, Conf, BLACK};
+mod math;
+
+use macroquad::prelude::{clear_background, draw_text, draw_texture, get_frame_time, next_frame, Conf, Image, Texture2D, BLACK, BLANK, GREEN, WHITE};
+use crate::math::{convert_iteration_array, get_iteration_field, ComplexNumber};
 
 /// Width of the window in stand-alone mode.
 const WINDOW_WIDTH: i32 = 1280;
@@ -11,7 +14,7 @@ const WINDOW_HEIGHT: i32 = 1024;
 fn window_conf() -> Conf {
     Conf {
         window_title: "Mandelbort".to_owned(),
-        window_width: WINDOW_WIDTH,
+        window_width: WINDOW_WIDTH ,
         window_height: WINDOW_HEIGHT,
         fullscreen: false,
         ..Default::default()
@@ -20,11 +23,29 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
+
+    let center = ComplexNumber::new(-1.0, 0.0);
+    let radius = 2.0;
+
+    let mut image = Image::gen_image_color(WINDOW_WIDTH as u16, WINDOW_HEIGHT as u16, BLANK);
+    let texture = Texture2D::from_image(&image);
+
+
+
     loop {
         let delta_time = get_frame_time();
 
         clear_background(BLACK);
 
+        let num_array = get_iteration_field(center.clone(), radius);
+        let color_array = convert_iteration_array(&num_array);
+
+        image.update(&color_array);
+        texture.update(&image);
+        draw_texture(&texture, 0.0, 0.0, WHITE);
+
+        let time_str = format!("Zeit: {:.2}s", delta_time);
+        draw_text(&time_str, 20.0, 50.0, 30.0, GREEN);
 
         next_frame().await;
     }
---
+++ src/math.rs
@@ -0,0 +1,112 @@
+use rayon::prelude::*;
+use macroquad::prelude::{Color, BLACK};
+use crate::{WINDOW_HEIGHT, WINDOW_WIDTH};
+
+/// The maximum amount of iterations we want to do for a complex number in Mandelbbrot to check for divergence.
+const MAX_ITER:u16 = 1000;
+
+/// Complex number used in Mandelbrot in double precision.
+#[derive(Default,Debug,Clone)]
+pub struct ComplexNumber {
+    pub real: f64,
+    pub imag: f64,
+}
+
+
+impl ComplexNumber {
+    /// Constructor.
+    pub fn new(real: f64, imag: f64) -> ComplexNumber {
+        ComplexNumber { real, imag }
+    }
+
+    /// Checks if we are already too large to continue iteration.
+    fn is_too_large(&self) -> bool {
+        self.real * self.real + self.imag * self.imag > 2.0 * 2.0
+    }
+
+    /// Does the next step on a complex number.
+    fn next_step(&mut self, offset: &ComplexNumber) {
+        (self.real, self.imag) = (self.real * self.real - self.imag * self.imag + offset.real,
+                                  2.0 * self.real * self.imag + offset.imag);
+    }
+
+    /// Gets the amount of iterations we need till divergence.
+    pub fn get_iteration_till_termination(&self) -> u16 {
+        let mut iter = 0;
+        let mut scan = ComplexNumber::default();
+        while iter < MAX_ITER && !scan.is_too_large() {
+            scan.next_step(&self);
+            iter += 1;
+        }
+        iter
+    }
+
+    pub fn add_into(&mut self, other: &ComplexNumber) {
+        self.real += other.real;
+        self.imag += other.imag;
+    }
+}
+
+
+/// Generates an iteration field for the given complex number as a center and an extension given as a radius.
+pub fn get_iteration_field(center: ComplexNumber, extension : f64) -> Vec<u16> {
+    let window_height = WINDOW_HEIGHT as f64;
+    let step_increment = extension / (window_height * 0.5);
+
+    (0..WINDOW_WIDTH * WINDOW_HEIGHT).into_par_iter().map(|x| {
+        let y_pos = x / WINDOW_WIDTH - WINDOW_HEIGHT / 2;
+        let x_pos = x % WINDOW_WIDTH - WINDOW_WIDTH / 2;
+        let mut scan = ComplexNumber::new(x_pos as f64 * step_increment, y_pos as f64 * step_increment);
+        scan.add_into(&center);
+        scan.get_iteration_till_termination()
+    }).collect::<Vec<u16>>()
+}
+
+
+/// Converts hsv to rgb color.
+fn hsv_to_rgb_color(h: f32, s: f32, v: f32) -> Color {
+    let mut r = 0.0;
+    let mut g = 0.0;
+    let mut b = 0.0;
+
+    if s == 0.0 {
+        r = v;
+        g = v;
+        b = v;
+    } else {
+        let h_i = (h * 6.0).floor();
+        let f = h * 6.0 - h_i;
+        let p = v * (1.0 - s);
+        let q = v * (1.0 - f * s);
+        let t = v * (1.0 - (1.0 - f) * s);
+
+        match h_i as i32 {
+            0 => { r = v; g = t; b = p; }
+            1 => { r = q; g = v; b = p; }
+            2 => { r = p; g = v; b = t; }
+            3 => { r = p; g = q; b = v; }
+            4 => { r = t; g = p; b = v; }
+            5 => { r = v; g = p; b = q; }
+            _ => {}
+        }
+    }
+    Color::new(r, g, b, 1.0)
+}
+
+/// The amount of complete cycles we do on the hue for the complete stretch.
+const HUE_CYCLES : f32 = 5.0;
+/// The light intensity we use on the color.
+const COLOR_VALUE : f32 = 0.8;
+/// The color saturation we use.
+const COLOR_SATURATION : f32 = 0.7;
+
+
+/// Takes a field with iterations and converts it into a color array.
+pub fn convert_iteration_array(in_field: &[u16]) -> Vec< Color> {
+    in_field.par_iter().map( |i| {
+        if *i == MAX_ITER {BLACK} else {
+            let rel_val = (*i as f32 * HUE_CYCLES / MAX_ITER as f32).fract();
+            hsv_to_rgb_color(rel_val, COLOR_SATURATION, COLOR_VALUE)
+        }
+    }).collect()
+}

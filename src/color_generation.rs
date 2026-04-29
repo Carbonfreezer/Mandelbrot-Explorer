9f9d800f6036960c8d6591f35ee47c0ad3516770
Christoph Lürig <christoph.luerig@gmail.com>
2026-04-29 21:36:35 +0200

    better

--- src/color_generation.rs
+++ src/color_generation.rs
@@ -1,5 +1,6 @@
 //! Module is responsible for mapping the iteration field to a color value. It us using a cyclicle color map here.
 
+use colorous::{MAGMA, VIRIDIS};
 use rayon::iter::ParallelIterator;
 use rayon::iter::IndexedParallelIterator;
 use crate::effect_system::{create_low_pass_filtered_density_field, create_shading_field};
@@ -7,8 +8,6 @@ use crate::math::MAX_ITER;
 use macroquad::color::{Color, hsl_to_rgb};
 use rayon::prelude::IntoParallelRefIterator;
 
-/// The amount of complete cycles we do on the hue for the complete stretch.
-const HUE_CYCLES: f32 = 1.0;
 /// The light intensity we use on the color.
 const COLOR_VALUE: f32 = 0.8;
 /// The color saturation we use.
@@ -25,8 +24,10 @@ pub fn generate_colors(in_field: &[f32]) -> Vec<Color> {
 
     in_field.par_iter().zip(shading.par_iter()).map(|(l, r)|
         {
-            let rel_val = (l *HUE_CYCLES / MAX_ITER as f32).fract();
-            hsl_to_rgb(rel_val, COLOR_SATURATION, r * COLOR_VALUE * f32::exp(- 1.5 *  (MAX_ITER as f32 - l) / MAX_ITER as f32) )
+            let t =  (l / MAX_ITER as f32).sqrt() as f64;
+            let c = MAGMA.eval_continuous(t);
+            let brightness =  r * f32::exp(-0.5 * (MAX_ITER as f32 - l) / MAX_ITER as f32);
+            Color::new(brightness * c.r as f32 / 255.0, brightness *c.g as f32 / 255.0, brightness *c.b as f32 / 255.0, 1.0)
         }).collect()
 
 }

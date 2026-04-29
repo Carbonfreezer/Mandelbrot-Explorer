f57fdc5a0fcb1dbf5a0fc4b7429cb281320a598d
Christoph Lürig <christoph.luerig@gmail.com>
2026-04-29 20:29:35 +0200

    better

--- src/color_generation.rs
+++ src/color_generation.rs
@@ -1,32 +1,19 @@
 //! Module is responsible for mapping the iteration field to a color value. It us using a cyclicle color map here.
 
+use rayon::iter::ParallelIterator;
+use rayon::iter::IndexedParallelIterator;
 use crate::effect_system::{create_low_pass_filtered_density_field, create_shading_field};
 use crate::math::MAX_ITER;
-use macroquad::color::{BLACK, Color, hsl_to_rgb};
-use std::sync::LazyLock;
+use macroquad::color::{Color, hsl_to_rgb};
+use rayon::prelude::IntoParallelRefIterator;
 
 /// The amount of complete cycles we do on the hue for the complete stretch.
-const HUE_CYCLES: f32 = 2.0;
+const HUE_CYCLES: f32 = 1.0;
 /// The light intensity we use on the color.
 const COLOR_VALUE: f32 = 0.8;
 /// The color saturation we use.
 const COLOR_SATURATION: f32 = 0.8;
 
-/// The lookup table for all entries as static array.
-static COLOR_ARRAY: LazyLock<Vec<Color>> = LazyLock::new(create_all_colors);
-
-/// Helper function to build the lookup table.
-fn create_all_colors() -> Vec<Color> {
-    let mut vec: Vec<_> = (0..MAX_ITER)
-        .map(|i| {
-            let rel_val = (i as f32 * HUE_CYCLES / MAX_ITER as f32).fract();
-            hsl_to_rgb(rel_val, COLOR_SATURATION, (i as f32 / MAX_ITER as f32) * COLOR_VALUE)
-        })
-        .collect();
-   // vec.push(BLACK);
-    vec.push( hsl_to_rgb(1.0, COLOR_SATURATION, COLOR_VALUE));
-    vec
-}
 
 
 
@@ -35,15 +22,11 @@ pub fn generate_colors(in_field: &[u16]) -> Vec<Color> {
     let low_pass = create_low_pass_filtered_density_field(in_field);
     let shading = create_shading_field(&low_pass);
 
-    in_field
-        .iter()
-        .zip(shading)
-        .map(|(i, luminance)| {
-            let mut color = COLOR_ARRAY[*i as usize];
-            color.r *= luminance;
-            color.g *= luminance;
-            color.b *= luminance;
-            color
-        })
-        .collect()
+
+    low_pass.par_iter().zip(shading.par_iter()).map(|(l, r)|
+        {
+            let rel_val = (l *HUE_CYCLES / MAX_ITER as f32).fract();
+            hsl_to_rgb(rel_val, COLOR_SATURATION, r * COLOR_VALUE * f32::exp(- 1.5 *  (MAX_ITER as f32 - l) / MAX_ITER as f32) )
+        }).collect()
+
 }
